c31085747ffd0f5f4edfa0eadbe454b06e13fd76
ODINA101 <iraklisamniashviliii@gmail.com>
2019-06-27 15:08:57 +0400

    animations finished

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, ImageBackground, Dimensions, PanResponder } from 'react-native';
+import { TouchableHighlight, BackHandler, Button, Platform, StyleSheet, Text, View, ImageBackground, Dimensions, PanResponder, Modal } from 'react-native';
 import SvgComponent from "./svgc"
 const { width, height } = Dimensions.get('window');
 import ReactNativeZoomableView from '@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView';
@@ -7,18 +7,43 @@ import ReactNativeZoomableView from '@dudigital/react-native-zoomable-view/src/R
 
 
 export default class App extends Component {
-
-
+  state = {
+    modalVisible: false,
+  };
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      this.refs.svgC.back()
+      return true;
+    });
+  }
 
   render() {
-
     return (
-      <View style={styles.container} >
-
+      <View style={styles.container}>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}>
+          <View style={{ marginTop: 22 }}>
+            <View>
+              <Text>Hello World!</Text>
+              <Button
+                onPress={() => this.setState({ modalVisible: false })}
+                title="Learn More"
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+              />
+            </View>
+          </View>
+        </Modal>
         <SvgComponent
+          showModal={() => this.setState({ modalVisible: true })}
+          ref='svgC'
           width={width}
           height={height}
-
         />
       </View >
     );
